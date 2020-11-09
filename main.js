59bed34b3f8b1c4fcb02deaed305472c3e4b53c2
xelopsys <xelopsys@users.noreply.github.com>
2020-11-09 07:20:46 +0500

    fixed

---
+++ main.js
@@ -0,0 +1,23 @@
+// const chatArea = document.getElementById("row");
+// const input = document.getElementById("input");
+// const btn = document.getElementById("btn");
+
+
+
+
+// btn.addEventListener("click", () => {
+//     let userInput = input.value;
+
+//     let temp = `<div class="msg">
+//     <span class="my-msg">${userInput}</span>
+//     <img src="img.jpg" class="avatar">
+//     </div>`;
+
+//     chatArea.insertAdjacentHTML('beforeend', temp)
+
+// })
+
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
